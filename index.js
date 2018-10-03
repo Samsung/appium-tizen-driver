daf42ac8ceab7e30555700bccdeda06034861f34
Isaac Murchie <isaac@saucelabs.com>
2018-10-03 16:22:01 -0400

    Fix exports for new Babel

--- index.js
+++ index.js
@@ -3,11 +3,18 @@
 
 import yargs from 'yargs';
 import { asyncify } from 'asyncbox';
-import { startServer } from './lib/server';
-import TizenDriver from './lib/driver';
-import tizenHelpers from './lib/tizen-helpers';
-import tizenCommands from './lib/commands/index';
-import { commonCapConstraints } from './lib/desired-caps';
+import * as server from './lib/server';
+import * as driver from './lib/driver';
+import * as helpers from './lib/tizen-helpers';
+import * as commands from './lib/commands/index';
+import * as caps from './lib/desired-caps';
+
+
+const { startServer } = server;
+const { TizenDriver } = driver;
+const { helpers: tizenHelpers } = helpers;
+const { commands: tizenCommands } = commands;
+const { commonCapConstraints } = caps;
 
 const DEFAULT_HOST = "localhost";
 const DEFAULT_PORT = 4723;
---
+++ test/driver-specs.js
@@ -0,0 +1,8 @@
+import TizenDriver from '..';
+
+
+describe('TizenDriver', function () {
+  it('should be instantiable', function () {
+    new TizenDriver();
+  });
+});
